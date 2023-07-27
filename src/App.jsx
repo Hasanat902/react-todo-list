33d9d84cb25ed2324ddf119c481d597c2a5c0599
Hasanat902 <abulhasanat902@gmail.com>
2023-07-27 12:34:21 +0600

    added design

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
-export default function App(){
-  return 'Hi'
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
